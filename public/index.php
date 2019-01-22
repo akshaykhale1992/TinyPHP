b3be3843fbab10cc5d2d5c18b6152d042e0ec596
Akshay Khale <akshaykhale1992@users.noreply.github.com>
2019-01-22 12:02:11 +0530

    Updated Index.php and adjusted spacing

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 /**
  * Loading composer Libraries
  */
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ .'/../vendor/autoload.php';
 
 
 /**
@@ -24,11 +24,11 @@ $dotenv->load();
 require __DIR__.'/../helpers.php';
 
 require __DIR__.'/../'.getenv('CONTROLLERS_DIRECTORY','controllers').'/Controller.php';
-$paths = array(__DIR__ . '/../'.getenv('VIEWS_DIRECTORY','views'));
-$cache_path = array('cache_path' => __DIR__ . '/../'.getenv('CACHE_DIRECTORY','cache'));
+$paths = array(__DIR__ .'/../'.getenv('VIEWS_DIRECTORY','views'));
+$cache_path = array('cache_path' => __DIR__ .'/../'.getenv('CACHE_DIRECTORY','cache'));
 
 $path = explode('/', trim(strtolower($_SERVER['REQUEST_URI']), '/'));
-$path[count($path)-1] = explode('?', end($path))[0];// Removing get parameters from the Request URL
+$path[count($path)-1] = explode('?', end($path))[0]; // Removing get parameters from the Request URL
 
 $default_pages = ['index','home','default']; //Default pages of the website
 
@@ -39,7 +39,7 @@ if (empty($path[0]) || in_array($path[0], $default_pages)) {
 }
 
 $controller = new Controller($paths, $cache_path);
-if (method_exists ($controller, $method)) {
+if (method_exists($controller, $method)) {
 	echo $controller->{ $method }(); // Calling the method from the Controller
 } else {
 	echo $controller->notFound(); // Calling 404 Page if method not found
